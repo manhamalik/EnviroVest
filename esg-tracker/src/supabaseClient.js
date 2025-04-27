0f197a3e36868bf4685340741fcf45e289354a90
Manha Malik <manhamalik77@gmail.com>
2025-04-27 17:32:30 -0400

    updates

--- esg-tracker/src/supabaseClient.js
+++ esg-tracker/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://zwfponltzmrnwcgjevik.supabase.co/'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp3ZnBvbmx0em1ybndjZ2pldmlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIwNzA4NjksImV4cCI6MjA1NzY0Njg2OX0.efK6dWbpOLIlGb-4ORnIYmiiyjg11gCnB1gGquC2lH8'
+const supabaseUrl = 'https://qphrgqkvzxsrjynpkasm.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwaHJncWt2enhzcmp5bnBrYXNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NDc0NzEsImV4cCI6MjA2MTIyMzQ3MX0.6KxWI17z7l4DMJ43CDDK_UMJ-w1IDjderBq24EQhrSM'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
